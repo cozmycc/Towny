5ce6d58aefbad5d87669d3fa816927e339366208
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2024-02-18 16:19:40 +0100

    Remove missed desugars

--- Towny/src/main/java/com/palmergames/bukkit/towny/object/CellSurface.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/object/CellSurface.java
@@ -10,7 +10,6 @@ import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import com.github.bsideup.jabel.Desugar;
 import com.palmergames.bukkit.towny.Towny;
 import com.palmergames.bukkit.towny.TownySettings;
 import com.palmergames.bukkit.towny.utils.BorderUtil;
@@ -29,7 +28,6 @@ public class CellSurface {
 		return new CellSurface(worldCoord);
 	}
 
-	@Desugar
 	public record BlockPos(int x, int z) {}
 
 	public void runClaimingParticleOverSurfaceAtPlayer(Player player) {
